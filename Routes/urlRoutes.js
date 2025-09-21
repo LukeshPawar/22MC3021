ea581684ad8e289cfc66e0297f94ee109547f9ba
LUKESH PAWAR <157102729+LukeshPawar@users.noreply.github.com>
2025-09-21 16:26:02 +0530

    Edit

--- Routes/urlRoutes.js
+++ Routes/urlRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { createShortUrl, getStats, redirectUrl } = require("../Controllers/urlController");
 
 router.post("/shorturls", createShortUrl);
-router.get("/stats/:shortId", getStats);
+router.get("/shorturls/:shortId", getStats);
 router.get("/:shortId", redirectUrl);
 
 module.exports = router;
